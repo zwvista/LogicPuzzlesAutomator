d644cedfffc4b3382a82fb7ff14899053d9ef189
趙偉 <zwvista@msn.com>
2025-10-09 20:18:06 +0900

    Snail 1

---
+++ Puzzles/Snail/main.py
@@ -0,0 +1,26 @@
+from typing import Self, override
+
+from Puzzles.puzzle_analyzer import PuzzleAnalyzer, to_base_36, get_level_str_from_matrix
+
+
+# Games 1 Puzzle Set 14
+class _Analyzer(PuzzleAnalyzer):
+
+    def __init__(self: Self):
+        super().__init__(
+            100,
+            [(1,5), (15,6), (26,7), (37,8), (47,9), (57,10), (77,11)],
+            True
+        )
+
+    @override
+    def get_level_str_from_image(self: Self) -> str:
+        horizontal_lines, vertical_lines = self.get_grid_lines_by_cell_count(self.cell_count)
+        matrix = self.recognize_digits(horizontal_lines, vertical_lines)
+        level_str = get_level_str_from_matrix(matrix, to_base_36)
+        return level_str
+
+
+analyzer = _Analyzer()
+# analyzer.take_snapshot()
+analyzer.get_levels_str_from_puzzle()
