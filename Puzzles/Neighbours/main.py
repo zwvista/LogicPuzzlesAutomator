37b93a52111962c5bf11d7009cab56d09decd89a
wei.zhao <wei.zhao@nsw.co.jp>
2025-10-14 18:38:14 +0900

    Neighbours 1

---
+++ Puzzles/Neighbours/main.py
@@ -0,0 +1,26 @@
+from typing import Self, override
+
+from Puzzles.puzzle_analyzer import PuzzleAnalyzer, to_base_36, get_level_str_from_matrix
+
+
+# Games 1 Puzzle Set 8
+class _Analyzer(PuzzleAnalyzer):
+
+    def __init__(self: Self):
+        super().__init__(
+            123,
+            [(1,4), (3,5), (49,6), (123,8)],
+            True
+        )
+
+    @override
+    def get_level_str_from_image(self: Self) -> str:
+        horizontal_lines, vertical_lines = self.get_grid_lines_by_cell_count(self.cell_count)
+        matrix = self.recognize_digits(horizontal_lines, vertical_lines)
+        level_str = get_level_str_from_matrix(matrix)
+        return level_str
+
+
+analyzer = _Analyzer()
+analyzer.take_snapshot()
+# analyzer.get_levels_str_from_puzzle()
