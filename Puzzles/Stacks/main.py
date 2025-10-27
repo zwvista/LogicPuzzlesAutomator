2a8b52e87680a800a9cbe1a7aa086851f030831d
趙偉 <zwvista@msn.com>
2025-10-28 02:37:11 +0900

    LoopAndBlocks

---
+++ Puzzles/Stacks/main.py
@@ -0,0 +1,28 @@
+from typing import Self, override
+
+from Puzzles.puzzle_analyzer import PuzzleAnalyzer, format_matrix_with_walls
+
+
+# Games 2 Puzzle Set 7
+class _Analyzer(PuzzleAnalyzer):
+
+    def __init__(self: Self):
+        super().__init__(
+            200,
+            [(1, 4), (6, 5), (31, 6), (61, 7), (101, 8), (141, 9), (171, 10)]
+        )
+
+    @override
+    def get_level_str_from_image(self: Self) -> str:
+        horizontal_lines, vertical_lines = self.get_grid_lines_by_cell_count(self.cell_count)
+        matrix = self.recognize_digits(horizontal_lines, vertical_lines)
+        walls = self.recognize_walls(horizontal_lines, vertical_lines)
+        level_str = format_matrix_with_walls(matrix, walls)
+        return level_str
+
+
+if __name__ == "__main__":
+    analyzer = _Analyzer()
+    # analyzer.take_snapshot(app_series_no=2)
+    # analyzer.get_level_board_size_from_puzzle()
+    analyzer.get_levels_str_from_puzzle()
