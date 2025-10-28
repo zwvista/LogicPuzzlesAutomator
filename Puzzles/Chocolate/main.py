0fe20761723fcc432b2c4372e61aee9e3cc69ad9
趙偉 <zwvista@msn.com>
2025-10-28 21:20:31 +0900

    Chocolate

---
+++ Puzzles/Chocolate/main.py
@@ -0,0 +1,28 @@
+from typing import Self, override
+
+from Puzzles.puzzle_analyzer import PuzzleAnalyzer, format_matrix_with_walls
+
+
+# Games 1 Puzzle Set 2
+class _Analyzer(PuzzleAnalyzer):
+
+    def __init__(self: Self):
+        super().__init__(
+            200,
+            [(1, 4), (11, 5), (41, 6), (61, 7), (91, 8), (121, 9), (141, 10)]
+        )
+
+    @override
+    def get_level_str_from_image(self: Self) -> str:
+        horizontal_lines, vertical_lines = self.get_grid_lines_by_cell_count(self.cell_count)
+        matrix = self.recognize_digits(horizontal_lines, vertical_lines)
+        walls = self.recognize_walls(horizontal_lines, vertical_lines)
+        level_str = format_matrix_with_walls(matrix, walls)
+        return level_str
+
+
+if __name__ == "__main__":
+    analyzer = _Analyzer()
+    # analyzer.take_snapshot(app_series_no=2)
+    # analyzer.get_level_board_size_from_puzzle()
+    analyzer.get_levels_str_from_puzzle()
