c43582599b474eb80bfc33d4cb2f31dbfe0485a5
趙偉 <zwvista@msn.com>
2025-11-05 01:46:00 +0900

    MineShips

---
+++ Puzzles/MineShips/main.py
@@ -0,0 +1,32 @@
+from typing import Self, override
+
+from Puzzles.puzzle_analyzer import PuzzleAnalyzer, get_level_str_from_matrix
+
+
+# Games 2 Puzzle Set 4
+class _Analyzer(PuzzleAnalyzer):
+
+    def __init__(self: Self):
+        super().__init__(
+            230,
+            [(1, 10), (111, 11), (191, 12)]
+        )
+
+    @override
+    def get_level_str_from_image(self: Self) -> str:
+        horizontal_lines, vertical_lines = self.get_grid_lines_by_cell_count(self.cell_count)
+        matrix = self.recognize_digits(horizontal_lines, vertical_lines)
+        level_str = get_level_str_from_matrix(matrix)
+        return level_str
+
+    @override
+    def get_attr_str_from_image(self: Self) -> str:
+        output = self.recognize_text(660, 56, 500, 34)
+        return f' SuperTanker="1"' if output else ''
+
+
+if __name__ == "__main__":
+    analyzer = _Analyzer()
+    # analyzer.take_snapshot()
+    # analyzer.get_level_board_size_from_puzzle()
+    analyzer.get_levels_str_from_puzzle()
