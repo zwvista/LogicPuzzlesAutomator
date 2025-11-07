db5d5b19a41d026d72cda9db08bae7d30f58fd6b
趙偉 <zwvista@msn.com>
2025-11-08 00:40:59 +0900

    NewCarpenterSquare

---
+++ Puzzles/NewCarpenterSquare/main.py
@@ -0,0 +1,51 @@
+from typing import Self, override
+
+from Puzzles.puzzle_analyzer import PuzzleAnalyzer, get_template_img_4channel_list, get_level_str_from_matrix
+
+
+# Games 1 Puzzle Set 10
+class _Analyzer(PuzzleAnalyzer):
+
+    DIFF_PATH = '../../images/different.png'
+    EQUAL_PATH = '../../images/equal.png'
+    QM_PATH = '../../images/help_transparent.png'
+    template_img_4channel_list = get_template_img_4channel_list(DIFF_PATH, EQUAL_PATH, QM_PATH)
+
+    def __init__(self: Self):
+        super().__init__(
+            300,
+            [(1, 5), (21, 6), (51, 7), (91, 8), (151, 9), (196, 10), (241, 11)]
+        )
+
+    def recognize_template(
+            self: Self,
+            horizontal_line_list: list[tuple[int, int]],
+            vertical_line_list: list[tuple[int, int]]
+    ) -> list[list[str]]:
+        result = []
+        for row_idx, (y, h) in enumerate(vertical_line_list):
+            row_result = []
+            for col_idx, (x, w) in enumerate(horizontal_line_list):
+                index = self.get_template_index_by_diff_in_region(
+                    template_img_4channel_list=self.template_img_4channel_list,
+                    top_left_coord=(x, y),
+                    size=(w, h),
+                )
+                ch = ' ' if index == -1 else '/=?'[index]
+                row_result.append(ch)
+            result.append(row_result)
+        return result
+
+    @override
+    def get_level_str_from_image(self: Self) -> str:
+        horizontal_lines, vertical_lines = self.get_grid_lines_by_cell_count(self.cell_count)
+        matrix = self.recognize_template(horizontal_lines, vertical_lines)
+        level_str = get_level_str_from_matrix(matrix)
+        return level_str
+
+
+if __name__ == "__main__":
+    analyzer = _Analyzer()
+    # analyzer.take_snapshot(app_series_no=3)
+    # analyzer.get_level_board_size_from_puzzle()
+    analyzer.get_levels_str_from_puzzle()
