abbd0c4fa788c52cf755c6862eb04884dc71d325
趙偉 <zwvista@msn.com>
2025-11-16 04:13:42 +0900

    SuspendedGravity

---
+++ Puzzles/SuspendedGravity/main.py
@@ -0,0 +1,39 @@
+from typing import Self, override
+
+import cv2
+
+from Puzzles.puzzle_analyzer import PuzzleAnalyzer, format_matrix_with_walls
+
+
+# Games 1 Puzzle Set 2
+class _Analyzer(PuzzleAnalyzer):
+
+    def __init__(self: Self):
+        super().__init__(
+            300,
+            [(1, 4), (21, 5), (31, 6), (121, 7), (181, 8), (231, 9), (241, 10), (281, 11)]
+        )
+
+    @override
+    def get_level_str_from_image(self: Self) -> str:
+        horizontal_lines, vertical_lines = self.get_grid_lines_by_cell_count(self.cell_count)
+        horizontal_line_results = self.analyze_horizontal_line(1370, 20, 1170)
+        if len(horizontal_line_results) == 1:
+            vertical_lines = vertical_lines[:-1]
+        vertical_line_results = self.analyze_vertical_line(1170, 210, 1270)
+        if len(vertical_line_results) == 1:
+            horizontal_lines = horizontal_lines[:-1]
+        gray = cv2.cvtColor(self.large_img_bgr, cv2.COLOR_BGR2GRAY)
+        _, self.large_img_bgr = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+        self.large_img_bgr = cv2.cvtColor(self.large_img_bgr, cv2.COLOR_GRAY2BGR)
+        matrix = self.recognize_digits(horizontal_lines, vertical_lines)
+        walls = self.recognize_walls2(horizontal_lines, vertical_lines, 255)
+        level_str = format_matrix_with_walls(matrix, walls)
+        return level_str
+
+
+if __name__ == "__main__":
+    analyzer = _Analyzer()
+    # analyzer.take_snapshot(3)
+    # analyzer.get_level_board_size_from_puzzle()
+    analyzer.get_levels_str_from_puzzle()
