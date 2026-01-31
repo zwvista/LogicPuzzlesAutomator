6df7d3fb9fd1e75f4b619911c90d0b2fd1b65a10
趙偉 <zwvista@msn.com>
2026-01-31 23:08:50 +0900

    OnlyBends

---
+++ Puzzles/AssemblyInstructions/main.py
@@ -0,0 +1,59 @@
+from typing import Self, override
+
+import cv2
+import numpy as np
+
+from Puzzles.puzzle_analyzer import PuzzleAnalyzer, get_level_str_from_matrix
+
+
+# Games 1 Puzzle Set 1
+class _Analyzer(PuzzleAnalyzer):
+
+    def __init__(self: Self):
+        super().__init__(
+            300,
+            [(1, 4), (6, 5), (31, 6), (61, 7), (101, 8), (151, 9), (201, 10), (251, 11)]
+        )
+
+    def recognize_chars(
+            self: Self,
+            horizontal_line_list: list[tuple[int, int]],
+            vertical_line_list: list[tuple[int, int]]
+    ) -> list[list[str]]:
+        def get_roi_large(roi: np.ndarray) -> np.ndarray:
+            scale = .1 * self.cell_count
+            roi_large = cv2.resize(roi, None, fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
+            return roi_large
+
+        result = []
+        for row_idx, (y, h) in enumerate(vertical_line_list):
+            row_result = []
+            for col_idx, (x, w) in enumerate(horizontal_line_list):
+                horizontal_line_results = self.analyze_horizontal_line(y_coord=y + h // 2, start_x=x + 10, end_x=x+w - 10)
+                if len(horizontal_line_results) == 1:
+                    ch = ' '
+                else:
+                    output = self.recognize_text(x, y, w, h, get_roi_large=get_roi_large)
+                    if output:
+                        _, ch, prob = output
+                        ch = 'G' if ch == "6" else ch
+                    else:
+                        ch = 'I'
+                row_result.append(ch)
+            result.append(row_result)
+        return result
+
+
+    @override
+    def get_level_str_from_image(self: Self) -> str:
+        horizontal_lines, vertical_lines = self.get_grid_lines_by_cell_count(self.cell_count)
+        matrix = self.recognize_chars(horizontal_lines, vertical_lines)
+        level_str = get_level_str_from_matrix(matrix)
+        return level_str
+
+
+if __name__ == "__main__":
+    analyzer = _Analyzer()
+    # analyzer.take_snapshot(app_series_no=3)
+    # analyzer.get_level_board_size_from_puzzle()
+    analyzer.get_levels_str_from_puzzle()
---
+++ Puzzles/OnlyBends/main.py
@@ -0,0 +1,48 @@
+from typing import Self, override
+
+import cv2
+
+from Puzzles.puzzle_analyzer import PuzzleAnalyzer, get_level_str_from_matrix
+
+
+# Games 1 Puzzle Set 12
+class _Analyzer(PuzzleAnalyzer):
+
+    EMPTY_PATH = '../../images/lawn_background.png'
+    template_img_4channel_empty = cv2.imread(EMPTY_PATH, cv2.IMREAD_UNCHANGED)
+
+    def __init__(self: Self):
+        super().__init__(
+            400,
+            [(1, 5), (21, 6), (51, 7), (81, 8), (181, 9), (321, 10)]
+        )
+
+    def recognize_template(
+            self: Self,
+            horizontal_line_list: list[tuple[int, int]],
+            vertical_line_list: list[tuple[int, int]]
+    ) -> list[list[str]]:
+        result = []
+        for row_idx, (y, h) in enumerate(vertical_line_list):
+            row_result = []
+            for col_idx, (x, w) in enumerate(horizontal_line_list):
+                diff_empty = self.get_template_diff_in_region(self.template_img_4channel_empty, (x + 20, y + 20), (w - 40, h - 40))
+                ch = 'O' if diff_empty == 1 else ' '
+                # print(diff_empty, ch)
+                row_result.append(ch)
+            result.append(row_result)
+        return result
+
+    @override
+    def get_level_str_from_image(self: Self) -> str:
+        horizontal_lines, vertical_lines = self.get_grid_lines_by_cell_count(self.cell_count)
+        matrix = self.recognize_template(horizontal_lines, vertical_lines)
+        level_str = get_level_str_from_matrix(matrix)
+        return level_str
+
+
+if __name__ == "__main__":
+    analyzer = _Analyzer()
+    # analyzer.take_snapshot(app_series_no=4)
+    # analyzer.get_level_board_size_from_puzzle()
+    analyzer.get_levels_str_from_puzzle()
