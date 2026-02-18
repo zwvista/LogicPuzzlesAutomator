b2e3ab7d1d30c00137487b8513b4b712a72c8e64
趙偉 <zwvista@msn.com>
2026-02-19 06:46:23 +0900

    Bridges

---
+++ Puzzles/Bridges/main.py
@@ -0,0 +1,26 @@
+from typing import Self, override
+
+from Puzzles.puzzle_analyzer import PuzzleAnalyzer, get_level_str_from_matrix
+
+
+class _Analyzer(PuzzleAnalyzer):
+
+    def __init__(self: Self):
+        super().__init__(
+            38,
+            [(1, 6), (3, 7), (8, 8), (14, 9), (19, 10), (27, 11), (31, 12), (35, 14), (37, 16), (38, 18)]
+        )
+
+    @override
+    def get_level_str_from_image(self: Self) -> str:
+        horizontal_lines, vertical_lines = self.get_grid_lines_by_cell_count(self.cell_count)
+        matrix = self.recognize_digits(horizontal_lines, vertical_lines)
+        level_str = get_level_str_from_matrix(matrix)
+        return level_str
+
+
+if __name__ == "__main__":
+    analyzer = _Analyzer()
+    # analyzer.take_snapshot()
+    # analyzer.get_level_board_size_from_puzzle()
+    analyzer.get_levels_str_from_puzzle()
